87b93fdfac375ce541c8f8a97dfc50c84ca12504
sera0330 <seralee0330@gmail.com>
2020-11-10 21:36:34 +0900

    MongoDB 연결

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://sera:qwer1234@cluster0.evabn.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
